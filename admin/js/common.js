271296f2034824c381412dcca3481d0b1d25018e
duanshuting <1579455685@qq.com>
2018-12-10 17:56:56 +0800

    完成了后台登录跳转首页的功能

--- admin/js/common.js
+++ admin/js/common.js
@@ -6,3 +6,13 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+
+	// 检测用户登录
+	$.ajax({
+		url: '/api/employee/checkRootLogin',
+		type: 'get',
+		success: function (info) {
+			if (info.error) location.href = '/admin/login.html';
+		}
+	});
